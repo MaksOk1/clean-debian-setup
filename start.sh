2858c88654981d9d38b3e6f0e8226319cef19b78
MaksOk1 <maksimzikiy54694@gmail.com>
2026-05-18 19:23:24 +0300

    Foulder fix

--- start.sh
+++ start.sh
@@ -9,10 +9,10 @@ USER=$1
 URL='https://raw.github.com/MaksOk1/clean-debian-setup/main'
 
 # Base installation of packages and oh-my-zsh configuration
-$(which bash) ./scripts/base/base-install.sh $USER
-$(which bash) ./scripts/base/full-install.sh
-$(which bash) ./scripts/base/make-changes.sh $USER $URL
-$(which bash) ./scripts/base/finish.sh $USER
+$(which bash) ./scripts/basic/base-install.sh $USER
+$(which bash) ./scripts/basic/full-install.sh
+$(which bash) ./scripts/basic/make-changes.sh $USER $URL
+$(which bash) ./scripts/basic/finish.sh $USER
 
 # Fastfetch set-up as motd message on login (ssh)
 $(which bash) ./scripts/ssh-fastfetch/install.sh
