b9eaca2ac88fac903ebe11dea1e5e08379df6134
MaksOk1 <maksimzikiy54694@gmail.com>
2026-05-19 03:55:39 +0300

    install.sh - 'sudo' should install automatically now

--- install.sh
+++ install.sh
@@ -20,17 +20,34 @@ if [ -n "$MISSING_DEPS" ]; then
     echo -e "\e[33m[WARNING]: Required utilities are missing, but it is required to continue: ${MISSING_DEPS}.\e[0m"
 
     if [ "$IS_AUTO" = "1" ]; then
-        install_make="Y"
+        install_deps="Y"
     else
         read -rp "Do you want to install them now? [Y/n]: " install_deps
         install_deps=${install_deps:-Y}
     fi
 
     if [[ "$install_deps" =~ ^[Yy]$ ]]; then
-        echo -e "\e[32mInstalling dependencies (${MISSING_DEPS})... Authentication required.\e[0m"
-        if sudo apt-get update && sudo apt-get install -y $MISSING_DEPS; then
-            echo -e "\e[32mDependencies successfully installed!\e[0m"
+        echo -e "\e[32mInstalling dependencies (${MISSING_DEPS})...\e[0m"
+
+        if command -v sudo >/dev/null 2>&1; then
+            echo -e "\e[32mAuthentication via 'sudo' required.\e[0m"
+            if sudo apt-get update && sudo apt-get install -y $MISSING_DEPS; then
+                echo -e "\e[32mDependencies successfully installed (via 'sudo')!\e[0m"
+            else
+                echo -e "\e[31m[ERROR]: Failed to install packages (via 'sudo').\e[0m"
+                exit 1
+            fi
+        elif command -v su >/dev/null 2>&1; then
+            echo -e "\e[33m[WARNING]: 'sudo' is missing. Using 'su'. Root password required!\e[0m"
+            APT_Y="" && if [ "$IS_AUTO" = "1" ]; then APT_Y="-y"; fi
+            if su -c "apt-get update && apt-get install $APT_Y $MISSING_DEPS"; then
+                echo -e "\e[32mDependencies successfully installed (via 'su')!\e[0m"
+            else
+                echo -e "\e[31m[ERROR]: Failed to install packages (via 'su').\e[0m"
+                exit 1
+            fi
         else
+            echo -e "\e[31m[ERROR]: Neither 'sudo' nor 'su' were found to elevate privileges. Cannot install dependencies.\e[0m"
             echo -e "\e[31m[ERROR]: Failed to install packages. Please install missing ones manually ($MISSING_DEPS).\e[0m\n"
             exit 1
         fi
